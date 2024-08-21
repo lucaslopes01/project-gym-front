c4af322915c84ca6813f703a7edf5e527ea7153f
lucaslopes01 <lucaslsantos159@gmail.com>
2024-08-21 16:37:07 -0300

    implementação com a api

--- func.js
+++ func.js
@@ -1,11 +1,19 @@
-let data = {
-  'dias_semana':6
-}
-fetch('http:127.0.0.1:8000/planilha', {
+const botao = document.querySelector('#enviar')
+botao.addEventListener('click',function(){
+const data = document.querySelector('.dias_semana').value
+chama_treino({'dias_semana':data})
+})
+
+function chama_treino(data)
+  {fetch('http://127.0.0.1:8000/planilha', {
     method: "POST",
     body: JSON.stringify(data),
-    headers: {"Content-type": "application/json;charset=UTF-8"}
+    headers: {"Content-type": "application/json; charset=UTF-8"}
   })
   .then(response => response.json()) 
-  .then(json => console.log(json))
-  .catch(error => console.log(error));
+  .then(json => {
+    const resultado = document.querySelector('.resultado')
+    resultado.innerHTML = json.teste
+  })
+  .catch(err => console.log(err));
+}
