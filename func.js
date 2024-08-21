b7f7d53e912e0a88d68c877812c92b011419a114
lucas <lucas@pop-os.localdomain>
2024-08-20 22:56:11 -0300

    post

--- func.js
+++ func.js
@@ -1,5 +1,9 @@
-fetch('http:127.0.0.1:8000/teste', {
-    method: "GET",
+let data = {
+  'dias_semana':6
+}
+fetch('http:127.0.0.1:8000/planilha', {
+    method: "POST",
+    body: JSON.stringify(data),
     headers: {"Content-type": "application/json;charset=UTF-8"}
   })
   .then(response => response.json()) 
